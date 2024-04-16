69261e536eb3d59f25f6e5b9ce5992acce2227ca
Mariana Lopes <marianaobergerlop3s@gmail.com>
2024-04-16 15:15:29 -0300

    pagina_cadastro

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ function mostarSenha(){
         btnShowPass.classList.replace('bi-eye-slash','bi-eye')
     }
 }
+
+function mostarSenhaCadastro(){
+    var inputPass = document.getElementById('senha_cadastro')
+    var btnShowPass = document.getElementById('btn_senha_cadastro')
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type', 'text')
+        btnShowPass.classList.replace('bi-eye','bi-eye-slash')
+    }else{
+        inputPass.setAttribute('type', 'password')
+        btnShowPass.classList.replace('bi-eye-slash','bi-eye')
+    }
+}
